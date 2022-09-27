a1ec9aababfe20ed51f5daceb2adffe8127be933
Lukas van den Berk <lukasvdberk@gmail.com>
2022-09-27 13:40:21 +0200

    added unhappy flow for login

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -10,4 +10,15 @@ describe('Login page', () => {
 
         cy.location('pathname').should('eq', '/')
     })
+
+    it('should show the login page and not login with invalid credentials', () => {
+        cy.visit('/login')
+        cy.get('#username').type('test340@mail.com')
+        cy.get('#password').type('Test123123412!')
+        cy.get('#submit-button').click()
+
+        cy.contains('Something went wrong')
+        cy.location('pathname').should('eq', '/login')
+        cy.location('pathname').should('not.eq', '/')
+    })
 })
